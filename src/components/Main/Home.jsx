17c76c14fceca67ceb8a7da59cd3716dbd7d52c4
Slavi Sokolov <133207921+TripleS01@users.noreply.github.com>
2024-05-14 16:23:53 +0300

    pic

--- src/components/Main/Home.jsx
+++ src/components/Main/Home.jsx
@@ -16,18 +16,18 @@ export default function Home() {
                 <div className="home-container">
 
                     <div className="home-card">
-                        <img src="./static/IMG_0928.jpg" alt="nekler" />
+                        <img src="static/IMG_0928.jpg" alt="nekler" />
 
                         <Link to={Path.Merch} className="button-home">From us for you</Link>
                     </div>
 
                     <div className="home-card">
-                        <img src="./static/IMG_0033.jpg" alt="nekler" />
+                        <img src="static/IMG_0033.jpg" alt="nekler" />
                         <Link to={Path.Services} className="button-home">Services</Link>
                     </div>
 
                     <div className="home-card">
-                        <img src="./static/IMG_4292.jpg" alt="nekler" />
+                        <img src="static/IMG_4292.jpg" alt="nekler" />
                         <Link to={Path.OurTeam} className="button-home">Our Team</Link>
                     </div>
                 </div>
